f5eb3479e6dbcc1dfb45748666822f60f37d30b2
Harshit V <100853494+harshitv804@users.noreply.github.com>
2022-10-09 13:14:55 +0530

    Update wine.py

--- wine.py
+++ wine.py
@@ -17,9 +17,9 @@ st.write(df.isnull().sum())
 
 st.plotly_chart(px.imshow(df.corr(), text_auto=True).update_layout(width=800,height=800))
 
-st.plotly_chart(px.scatter(df,y="density", x='fixed_acidity', color_discrete_sequence=['#87CEEB']).update_layout(width=800,height=800, plot_bgcolor='black'))
+st.plotly_chart(px.scatter(df,y="density", x='fixed_acidity', color_discrete_sequence=['red']).update_layout(width=800,height=800, plot_bgcolor='black'))
 
-st.plotly_chart(px.histogram(df,x="density", color_discrete_sequence=['#87CEEB']).update_layout(width=800,height=800,plot_bgcolor='black'))
-st.plotly_chart(px.histogram(df,x="alcohol", color_discrete_sequence=['#87CEEB']).update_layout(width=800,height=800,plot_bgcolor='black'))
-st.plotly_chart(px.histogram(df,x="pH", color_discrete_sequence=['#87CEEB']).update_layout(width=800,height=800,plot_bgcolor='black'))
-st.plotly_chart(px.histogram(df,x="quality", color_discrete_sequence=['#87CEEB']).update_layout(width=800,height=800,plot_bgcolor='black'))
+st.plotly_chart(px.histogram(df,x="density", color_discrete_sequence=['red']).update_layout(width=800,height=800,plot_bgcolor='black'))
+st.plotly_chart(px.histogram(df,x="alcohol", color_discrete_sequence=['red']).update_layout(width=800,height=800,plot_bgcolor='black'))
+st.plotly_chart(px.histogram(df,x="pH", color_discrete_sequence=['red']).update_layout(width=800,height=800,plot_bgcolor='black'))
+st.plotly_chart(px.histogram(df,x="quality", color_discrete_sequence=['red']).update_layout(width=800,height=800,plot_bgcolor='black'))
